e8d5c0fea2e51424f4898777b39bf7e4857f57ac
Mulm9828 <94980556+Mulm9828@users.noreply.github.com>
2021-12-07 16:43:21 +0700

    Add files via upload

---
+++ html/Belajar.py/cos.py
@@ -0,0 +1,13 @@
+from turtle import *
+
+bgcolor('black')
+hideturtle()
+speed(11)
+for i in range (120):
+    if i % 2 == 0 :
+        color("cyan")
+elsa:
+     color('magenta')
+      forward(i * 3)
+left(91)
+done()
